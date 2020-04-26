6763325fe7d60e0c87f0fbde2ae53f9d6e6ced1a
Johannes Mols <johannes.mols@googlemail.com>
2020-04-26 21:39:05 +0200

    Cleaned up the dashboard

--- src/pages/FileHeaders.vue
+++ src/pages/FileHeaders.vue
@@ -23,66 +23,54 @@
     <div class="row">
 
       <div class="col-12">
-        <chart-card title="Standard scripts"
-                    sub-title="Usage of scripts used on the entire Bitcoin blockchain"
-                    :chart-data="usersChart.data"
-                    :chart-options="usersChart.options">
-          <span slot="footer">
-            <i class="ti-reload"></i> Updated 3 minutes ago
-          </span>
-          <div slot="legend">
-            <i class="fa fa-circle text-info"></i> Open
-            <i class="fa fa-circle text-danger"></i> Click
-            <i class="fa fa-circle text-warning"></i> Click Second Time
+        <card title="Standard scripts" subTitle="Usage of scripts used on the entire Bitcoin blockchain">
+          <div class="card-body">
+            <p></p>
           </div>
-        </chart-card>
+          <hr>
+          <div class="stats">
+            <i class="ti-reload"></i> Updated just now
+          </div>
+        </card>
       </div>
 
-      <div class="col-md-6 col-12">
-        <chart-card title="OP_RETURN Protocols"
-                    sub-title="Publicly known protocols using the operator"
-                    :chart-data="preferencesChart.data"
-                    chart-type="Pie">
-          <span slot="footer">
-            <i class="ti-timer"></i> Campaign set 2 days ago</span>
-          <div slot="legend">
-            <i class="fa fa-circle text-info"></i> Open
-            <i class="fa fa-circle text-danger"></i> Bounce
-            <i class="fa fa-circle text-warning"></i> Unsubscribe
+      <div class="col-6">
+        <card title="OP_RETURN Protocols" subTitle="Publicly known protocols using the operator">
+          <div class="card-body">
+            <p></p>
           </div>
-        </chart-card>
+          <hr>
+          <div class="stats">
+            <i class="ti-reload"></i> Updated just now
+          </div>
+        </card>
       </div>
 
-      <div class="col-md-6 col-12">
-        <chart-card title="OP_RETURN Output Size"
-                    sub-title="The average size of outputs using the operator"
-                    :chart-data="activityChart.data"
-                    :chart-options="activityChart.options">
-          <span slot="footer">
-            <i class="ti-check"></i> Data information certified
-          </span>
-          <div slot="legend">
-            <i class="fa fa-circle text-info"></i> Tesla Model S
-            <i class="fa fa-circle text-warning"></i> BMW 5 Series
+      <div class="col-6">
+        <card title="OP_RETURN Output Size" subTitle="The average size of outputs using the operator">
+          <div class="card-body">
+            <p></p>
           </div>
-        </chart-card>
+          <hr>
+          <div class="stats">
+            <i class="ti-reload"></i> Updated just now
+          </div>
+        </card>
       </div>
 
     </div>
-
   </div>
 </template>
+
 <script>
-import { StatsCard, ChartCard } from "@/components/index";
+import { Card, StatsCard } from "@/components/index";
 import Chartist from 'chartist';
+
 export default {
   components: {
-    StatsCard,
-    ChartCard
+    Card,
+    StatsCard
   },
-  /**
-   * Chart data used to render stats, charts. Should be replaced with server data
-   */
   data() {
     return {
       statsCards: [
@@ -118,79 +106,11 @@ export default {
           footerText: "Updated now",
           footerIcon: "ti-reload"
         }
-      ],
-      usersChart: {
-        data: {
-          labels: [
-            "9:00AM",
-            "12:00AM",
-            "3:00PM",
-            "6:00PM",
-            "9:00PM",
-            "12:00PM",
-            "3:00AM",
-            "6:00AM"
-          ],
-          series: [
-            [287, 385, 490, 562, 594, 626, 698, 895, 952],
-            [67, 152, 193, 240, 387, 435, 535, 642, 744],
-            [23, 113, 67, 108, 190, 239, 307, 410, 410]
-          ]
-        },
-        options: {
-          low: 0,
-          high: 1000,
-          showArea: true,
-          height: "245px",
-          axisX: {
-            showGrid: false
-          },
-          lineSmooth: Chartist.Interpolation.simple({
-            divisor: 3
-          }),
-          showLine: true,
-          showPoint: false
-        }
-      },
-      activityChart: {
-        data: {
-          labels: [
-            "Jan",
-            "Feb",
-            "Mar",
-            "Apr",
-            "Mai",
-            "Jun",
-            "Jul",
-            "Aug",
-            "Sep",
-            "Oct",
-            "Nov",
-            "Dec"
-          ],
-          series: [
-            [542, 543, 520, 680, 653, 753, 326, 434, 568, 610, 756, 895],
-            [230, 293, 380, 480, 503, 553, 600, 664, 698, 710, 736, 795]
-          ]
-        },
-        options: {
-          seriesBarDistance: 10,
-          axisX: {
-            showGrid: false
-          },
-          height: "245px"
-        }
-      },
-      preferencesChart: {
-        data: {
-          labels: ["62%", "32%", "6%"],
-          series: [62, 32, 6]
-        },
-        options: {}
-      }
+      ]
     };
   }
 };
 </script>
-<style>
+
+<style lang="scss" scoped>
 </style>
