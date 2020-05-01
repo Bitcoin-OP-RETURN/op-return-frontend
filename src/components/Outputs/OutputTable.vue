2533b2f22881143245117f921cddca9ca0af4e31
Johannes Mols <johannes.mols@googlemail.com>
2020-05-01 16:20:52 +0200

    Formatting table outputs

--- src/components/Outputs/OutputTable.vue
+++ src/components/Outputs/OutputTable.vue
@@ -12,6 +12,9 @@
 </template>
 
 <script>
+import protocols from "@/assets/config/protocols";
+import fileheaders from "@/assets/config/fileheaders";
+
 export default {
     props: {
         data: Array,
@@ -30,24 +33,44 @@ export default {
                 {
                     key: "outasm",
                     label: "Content",
-                    sortable: false
+                    sortable: false,
+                    formatter: this.formatContent
                 },
                 {
                     key: "protocol",
                     label: "Protocol",
-                    sortable: false
+                    sortable: false,
+                    formatter: this.formatProtocol
                 },
                 {
                     key: "fileheader",
                     label: "File Header",
-                    sortable: false
+                    sortable: false,
+                    formatter: this.formatFileheader
                 }
             ]
         }
     },
     methods: {
+        isValidHexString(str) {
+            return str.match(/^[A-Fa-f0-9]+/) !== null;
+        },
         formatDate(value, key, item) {
             return new Date(value * 1000).toLocaleString();
+        },
+        formatContent(value, key, item) {
+            return item.outtype === "nonstandard" ? item.outasm :
+                item.outtype === "nulldata" ?
+                    item.protocol === "emptytx" ? item.outasm :
+                        this.isValidHexString(item.outasm.split(" ").pop()) ?
+                            new Buffer(item.outasm.split(" ").pop(), "hex").toString() : item.outasm
+                        : item.outasm;
+        },
+        formatProtocol(value, key, item) {
+            return protocols.protocols.find(prot => prot.value === value).text;
+        },
+        formatFileheaders(value, key, item) {
+            return fileheaders.fileheaders.find(fh => fh.value === value).text;
         }
     }
 }
