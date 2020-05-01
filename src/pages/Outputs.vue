6de9291cfc1ad6008ad8f6de975e41953ae94cf3
Johannes Mols <johannes.mols@googlemail.com>
2020-05-01 17:06:05 +0200

    Fully functional search

--- src/pages/Outputs.vue
+++ src/pages/Outputs.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <card title="Explore the world of OP_RETURN" subTitle="You can search for any transaction, block, or even content">
-            <SearchFilter :searchOptions="searchOptions" :isSearching="isSearching" @optionsChanged="optionsChanged" @search="search" />
+            <SearchFilter :searchOptions="searchOptions" :isSearching="isSearching" @optionsChanged="optionsChanged" @search="searchClicked" />
         </card>
         <card>
             <OutputTable :data="searchResults" :page="page" :totalPages="totalPages" />
@@ -42,9 +42,17 @@ export default {
             errorMessage: "We encountered an error. Please try again."
         }
     },
+    async mounted() {
+        await this.search();
+    },
     methods: {
         optionsChanged(options) {
             this.searchOptions = options.searchOptions;
+        },
+        async searchClicked() {
+            this.page = 1;
+            this.totalPages = 1;
+            await this.search();
         },
         async search() {
             this.isSearching = true;
@@ -53,24 +61,22 @@ export default {
             this.searchOptions.query = this.searchOptions.query.trim();
 
             if (this.isValidTxOrBlockHash(this.searchOptions.query)) {
-                if (this.searchOptions.query.startsWith("00000")) {
-                    await this.searchTxHash();
-                } else {
-                    await this.searchBlockHash();
-                }
+                await this.searchHash();
             } else {
                 await this.searchContent();
             }
 
             this.isSearching = false;
         },
-        async searchTxHash() {
+        async searchHash() {
             var searchParams = new URLSearchParams();
             searchParams.append("hash", this.searchOptions.query.toLowerCase());
             searchParams.append("page", this.page);
 
+            var endpoint = this.searchOptions.query.startsWith("00000") ? "blockhash" : "txhash";
+
             // Wait for the first request
-            var response = await axios.get(api.server + "/tx-outputs/blockhash?" + searchParams.toString());
+            var response = await axios.get(api.server + "/tx-outputs/" + endpoint + "?" + searchParams.toString());
             if (response.status == 200) {
                 this.searchResults = response.data;
             } else {
@@ -79,7 +85,7 @@ export default {
 
             // Execute second request in background and update data afterwards
             searchParams.set("page", Number(searchParams.get("page")) + 1);
-            var secondResponse = axios.get(api.server + "/tx-outputs/blockhash?" + searchParams.toString())
+            var secondResponse = axios.get(api.server + "/tx-outputs/" + endpoint + "?" + searchParams.toString())
                 .then(response => {
                     if (response.data.length > 0) {
                         this.searchResultsNextPage = response.data;
@@ -91,11 +97,70 @@ export default {
                     this.searchResultsNextPage = [];
                 });
         },
-        async searchBlockHash() {
-
-        },
         async searchContent() {
+            var query = api.server + "/tx-outputs/search?";
+            var nextQuery = query;
+            var searchParams = new URLSearchParams();
 
+            if (this.searchOptions.minDate.getTime() !== new Date("2009-01-01").getTime()) {
+                searchParams.append("min_time", Math.floor(this.searchOptions.minDate.getTime() / 1000));
+            }
+
+            if (new Date(this.searchOptions.maxDate.toISOString().substr(0, 10)).getTime() !== new Date(new Date().toISOString().substr(0, 10)).getTime()) {
+                searchParams.append("max_time", Math.floor(this.searchOptions.maxDate.getTime() / 1000));
+            }
+
+            if (!this.isEmptyOrSpaces(this.searchOptions.query) && this.searchOptions.query.length >= 3) {
+                searchParams.append("search", this.searchOptions.query.trim());
+                searchParams.append("format", this.searchOptions.encoded ? "encoded" : "hex");
+            }
+
+            if (this.searchOptions.descending) {
+                searchParams.append("sort", "desc");
+            } else {
+                searchParams.append("sort", "asc");
+            }
+
+            if (this.page > 0) {
+                searchParams.append("page", this.page);
+            }
+
+            query += searchParams.toString();
+            searchParams.set("page", Number(searchParams.get("page")) + 1);
+            nextQuery += searchParams.toString();
+
+            if (this.searchOptions.protocols.length > 0) {
+                var appendPr = "&protocol=" + this.searchOptions.protocols.join(",");
+                query += appendPr;
+                nextQuery += appendPr;
+            }
+
+            if (this.searchOptions.fileheaders.length > 0) {
+                var appendFh = "&fileheader=" + this.searchOptions.fileheaders.join(",");
+                query += appendFh;
+                nextQuery += appendFh;
+            }
+
+            // Wait for the first request
+            var response = await axios.get(query);
+            if (response.status == 200) {
+                this.searchResults = response.data;
+            } else {
+                this.showErrorMessage = true;
+            }
+
+            // Execute second request in background and update data afterwards
+            var secondResponse = axios.get(nextQuery)
+                .then(response => {
+                    if (response.data.length > 0) {
+                        this.searchResultsNextPage = response.data;
+                    } else {
+                        this.searchResultsNextPage = [];
+                    }
+                })
+                .catch(error => {
+                    this.searchResultsNextPage = [];
+                });
         },
         isEmptyOrSpaces(str) {
             return str === null || str.match(/^ *$/) !== null;
